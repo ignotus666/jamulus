89e5d41a30d52ef6aeb541378760bb16cda64a79
ignotus666 <darylo1@hotmail.com>
2025-05-31 21:46:27 +0200

    fix windows build

--- src/threadpool.h
+++ src/threadpool.h
@@ -41,7 +41,11 @@ public:
     CThreadPool() = default;
     CThreadPool ( size_t );
     template<class F, class... Args>
+#if __cplusplus >= 201703L
+    auto enqueue ( F&& f, Args&&... args ) -> std::future<typename std::invoke_result<F, Args...>::type>;
+#else
     auto enqueue ( F&& f, Args&&... args ) -> std::future<typename std::result_of<F ( Args... )>::type>;
+#endif
     ~CThreadPool();
 
 private:
@@ -87,9 +91,17 @@ inline CThreadPool::CThreadPool ( size_t threads ) : stop ( false )
 
 // add new work item to the pool
 template<class F, class... Args>
+#if __cplusplus >= 201703L
+auto CThreadPool::enqueue ( F&& f, Args&&... args ) -> std::future<typename std::invoke_result<F, Args...>::type>
+#else
 auto CThreadPool::enqueue ( F&& f, Args&&... args ) -> std::future<typename std::result_of<F ( Args... )>::type>
+#endif
 {
+#if __cplusplus >= 201703L
+    using return_type = typename std::invoke_result<F, Args...>::type;
+#else
     using return_type = typename std::result_of<F ( Args... )>::type;
+#endif
 
     auto task = std::make_shared<std::packaged_task<return_type()>> ( std::bind ( std::forward<F> ( f ), std::forward<Args> ( args )... ) );
 
