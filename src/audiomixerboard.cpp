280a3724ae937992afa31bd8d2301350d013f83f
ignotus666 <darylo1@hotmail.com>
2025-06-02 22:12:59 +0200

    Clang-format

--- src/audiomixerboard.cpp
+++ src/audiomixerboard.cpp
@@ -1335,7 +1335,7 @@ void CAudioMixerBoard::ApplyNewConClientList ( CVector<CChannelInfo>& vecChanInf
     Mutex.unlock(); // release mutex
 
     // Ensure MIDI state is applied to faders during the connection process
-    SetMIDICtrlUsed(pSettings->bUseMIDIController);
+    SetMIDICtrlUsed ( pSettings->bUseMIDIController );
 
     // sort the channels according to the selected sorting type
     ChangeFaderOrder ( eChSortType );
--- src/clientsettingsdlg.cpp
+++ src/clientsettingsdlg.cpp
@@ -910,7 +910,7 @@ void CClientSettingsDlg::showEvent ( QShowEvent* event )
     chbUseMIDIController->setChecked ( pSettings->bUseMIDIController );
 
     // Emit MIDIControllerUsageChanged signal to propagate MIDI state at startup
-    emit MIDIControllerUsageChanged(chbUseMIDIController->isChecked());
+    emit MIDIControllerUsageChanged ( chbUseMIDIController->isChecked() );
 
     QDialog::showEvent ( event );
 }
