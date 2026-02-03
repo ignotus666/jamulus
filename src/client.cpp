ca2edc4b44c88f7c23d70f089402cc13c4eec639
ignotus666 <darylo1@hotmail.com>
2026-02-03 10:16:50 +0100

    Fix android build

--- src/client.cpp
+++ src/client.cpp
@@ -51,7 +51,11 @@ CClient::CClient ( const quint16  iPortNumber,
     bMuteOutStream ( false ),
     fMuteOutStreamGain ( 1.0f ),
     Socket ( &Channel, iPortNumber, iQosNumber, "", bNEnableIPv6 ),
+#ifdef ANDROID
+    Sound ( AudioCallback, this, bNoAutoJackConnect ),
+#else
     Sound ( AudioCallback, this, bNoAutoJackConnect, strNClientName ),
+#endif
     iAudioInFader ( AUD_FADER_IN_MIDDLE ),
     bReverbOnLeftChan ( false ),
     iReverbLevel ( 0 ),
